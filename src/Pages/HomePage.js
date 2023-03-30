5fb63248ed86dac435f53bea2a9e9eaffaf67d49
Elion Marku <71005986+ElionMarku00@users.noreply.github.com>
2023-03-30 22:42:51 +0200

    modified homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -12,76 +12,109 @@ function HomePage() {
 
     const { data } = React.useContext(AppContext)
 
-    return (
-        <Layout>
+    return (<Layout>
 
-            <img src={`/images/marielukas.png`} alt={`marielukas`} width="100%" height="auto" />
-
+            <Img src={`/images/marielukas.png`} alt={`marielukas`} width="100%" height="auto" />
             <Title>DOCTOR FOR 1 DAY</Title>
-            <CustomH6>Help Lucas and Marie find the code word the doctor needs to help the patient. With this code word you receive a lovely gift ! Choose the image below that matches the activity and answer the questions to find the code word . Good luck!
-            </CustomH6>
+        {/* <Flex>
+        </Flex > */}
 
-            <Grid>
 
-                {data.map((item) => {
+        <CustomH6>Help Lucas and Marie find the code word the doctor needs to help the patient. With this code word you receive a lovely gift ! Choose the image below that matches the activity and answer the questions to find the code word . Good luck!
+        </CustomH6>
+        <Grid>
 
-                    let { name, solved, letter, icon, text, type } = item
+            {data.map((item) => {
 
-                    return (<ImgContainer key={name}
-                        onClick={() => {
-                            !solved
-                                ? navigate(`/game`, { state: { text, currentGame: name, type, gameHost: Math.random() < 0.5 ? '/Lukas.png' : '/Marie.png' } })
-                                : navigate(`/correct`)
-                        }}>
-                        {!solved
-                            ? <img src={`/images${icon}`} alt={`${name}`} width="80%" height="auto" />
-                            : <ImgContainer>{letter}</ImgContainer>
-                        }
-                    </ImgContainer>)
+                let { name, solved, letter, icon, text, type } = item
 
-                })}
+                return (<ImgContainer key={name}
+                    onClick={() => {
+                        !solved
+                            ? navigate(`/game`, { state: { text, currentGame: name, type, gameHost: Math.random() < 0.5 ? '/Lukas.png' : '/Marie.png' } })
+                            : navigate(`/correct`)
+                    }}>
+                    {!solved
+                        ? <img src={`/images${icon}`} alt={`${name}`} width="80%" height="auto" />
+                        : <ImgContainer>{letter}</ImgContainer>
+                    }
+                </ImgContainer>)
 
-            </Grid>
+            })}
 
-            <BottomText onClick={() => navigate('/guesscode')}>
-                WHAT IS THE CODE WORD WE ARE LOOKING FOR?
-            </BottomText>
+        </Grid>
 
-        </Layout>
+        <BottomText onClick={() => navigate('/guesscode')}>
+            WHAT IS THE CODE WORD WE ARE LOOKING FOR?
+        </BottomText>
+
+    </Layout>
 
     )
 }
 
-const Layout = styled.section`
+const Layout = styled.div`
     display: grid;
-    grid-template-columns: 0.5fr 2fr;
-    background: lightblue;
+    grid-template-columns:repeat(5,1fr);
+    grid-template-rows:0.1fr 0.1fr 4fr 0.2fr;
     height: 100vh; 
     width: 100vw;
-    /* align-items: center; */
-    border-radius:1rem;
+    margin:0;
+    row-gap:0;
+
 `;
 
-const Grid = styled.main`
+const Grid = styled.div`
 
     display:grid;
     grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr;
-    grid-gap:3rem;
-    grid-column: 1 / 3;
-    align-items:center;
+    
+
+    align-items:center; 
     justify-self: center;
     justify-items:center;
     
     overflow-y:scroll;
+    grid-area:3/1/5/6;
+`;
+
+// const Flex = styled.div`
+
+//     display:flex;
+//     height:30%;
+//     max-height: 150px;
+//     flex-direction:row;
+//     flex-wrap:nowrap;
+//     align-items:center;
+//     justify-content: space-around;
+//     padding:10px;
+
+//     grid-area:1/1/1/6;
+
+//     object-fit: cover;
+//   vertical-align: bottom;
+
+// `;
+
+const Img = styled.img`
+
+height:100%;
+width:auto;
+max-height: 100%;
+grid-area:1/1/2/2;
+max-height:100px;
 
 `;
 
-const CustomH6 = styled.h6`
- grid-column: 1 / 3;
+const CustomH6 = styled.h4`
+ 
  padding:1rem;
 
  text-align:center;
+ align-self:center;
+ justify-self:center;
+
+ grid-area:2/1/2/6;
 
 `;
 
@@ -89,13 +122,18 @@ const Title = styled.h1`
 
 align-self:center;
 justify-self:center;
+text-align:center;
+grid-area:1/2/1/5;
 
 `;
 
-const BottomText = styled.h3`
+const BottomText = styled.h5`
 
 grid-column: 1 / 3;
 text-align:center;
+ align-self:center;
+ justify-self:center;
+grid-area:6/2/6/5;
 
 `;
 
