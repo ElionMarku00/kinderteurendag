9a75bed905a5f09d1206c88dd12bccadf8121d5e
Elion Marku <71005986+ElionMarku00@users.noreply.github.com>
2023-04-14 08:40:58 +0200

    set-up hosting in firebase

--- src/context.js
+++ src/context.js
@@ -6,12 +6,12 @@ const AppProvider = ({ children }) => {
 
   const [data, setData] = React.useState([
     { name: "Bee", icon: '/bee.png', letter: "E", type: GameTypes.multipleChoice, choices: { 'choise1': true, 'choise2': false }, answer: "choice2", solved: false, text: "Welcome to the sperm game! Do you know what you can fish up here?" },
-    { name: "Butterfly", icon: '/butterfly.png', type: GameTypes.multipleChoice, choices: { 'choise1': true, 'choise2': false }, letter: "M", answer: "test", solved: false, text: "Here you can perform an operation yourself like a real doctor! Do you want to give it a try? " },
-    { name: "Ladybug", icon: '/Ladybug.png', type: GameTypes.drag, letter: "B", answer: "test", solved: false, text: "On the door you can find a number. This is the number of ultrasounds we performed in 2022. " },
-    { name: "Slug", icon: '/Slug.png', type: GameTypes.text, letter: "R", answer: "test", solved: false, text: "Here you can see a lab person bringing the sperm cell into the egg cell with a very small needle. " },
-    { name: "Chick", icon: '/Chick.png', type: GameTypes.text, letter: "Y", answer: "test", solved: false, text: "Listen very well to what the doctor says." },
+    { name: "Butterfly", icon: '/butterfly.png', type: GameTypes.text, letter: "M", answer: "test", solved: false, text: "Here you can perform an operation yourself like a real doctor! Do you want to give it a try? " },
+    { name: "Ladybug", icon: '/Ladybug.png', type: GameTypes.text, letter: "B", answer: "test", solved: false, text: "Welcome to the ultrasound! \n On the door you can find a number. This is the number of ultrasounds we performed in 2022. " },
+    { name: "Slug", icon: '/Slug.png', type: GameTypes.drag, letter: "R", choices: { 'choise1': true, 'choise2': false }, answer: "test", solved: false, text: "Welcome to the lab Demo! \n Here you can see a lab person bringing the sperm cell into the egg cell with a very small needle. " },
+    { name: "Chick", icon: '/Chick.png', type: GameTypes.multipleChoice, choices: { 'choise1': true, 'choise2': false },answer: "test", letter: "Y", solved: false, text: "Welcome to the auditorium! \n Listen very well to what the doctor says." },
     {
-      name: "Plane", icon: '/Plane.png', type: GameTypes.text, letter: "O", answer: "test", solved: false, text: "What can you freeze here?"
+      name: "Plane", icon: '/Plane.png', type: GameTypes.text, letter: "O", choices: { 'choise1': true, 'choise2': false }, answer: "test", solved: false, text: "Welcome to the freezing demo! \n What can you freeze here?"
     },
   ])
 
