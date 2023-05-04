250123e9b65721e2cd7f95bc6f5ab9b5ccf19e50
Elion Marku <71005986+ElionMarku00@users.noreply.github.com>
2023-05-04 23:06:44 +0200

    fixed bug where ddl would strike through label on Initialscreen

--- src/Components/DropDownList.js
+++ src/Components/DropDownList.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import { OutlinedInput } from '@mui/material';
 import Select from '@mui/material/Select';
 import { AppContext } from '../context';
 import { useTranslation } from 'react-i18next';
@@ -15,15 +16,18 @@ export default function BasicSelect() {
     return (
         <Box style={{ width: '70vw' }}>
             <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">{t("languageQstn")}</InputLabel>
+                <InputLabel id="demo-simple-select-label" >{t("languageQstn")}</InputLabel>
+
                 <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    defaultValue="<MenuItem value={'nl'} >NL</MenuItem>"
+                    // defaultValue="<MenuItem value={'nl'} >NL</MenuItem>"
+                    defaultValue="nl"
                     // value={localStorage.getItem('language') || 'nl'}
                     // value={i18n.language || 'nl'}
                     value={i18n.language}
-                    label="Age"
+                    label="language"
+                    input={<OutlinedInput notched label={t("languageQstn")} />}
                     onChange={
                         (e) => {
                             setLanguage(e.target.value)
